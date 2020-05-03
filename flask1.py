a01c4f576aef29a11ac2377e7f0e0c8e7cdafbb0
nhatnguyen12 <ndminhnhat@gmail.com>
2020-05-02 21:45:30 -0400

    Deploy Machine Learning & NLP

---
+++ flask1.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    return "Hello World"
+# def add():
+#     a = request.form['a']
+#     b = request.form['b']
+#     return str(int(a) + int(b))
+
+
+if __name__ == '__main__':
+    app.run(port=7000)
